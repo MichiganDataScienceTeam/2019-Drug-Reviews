c9d838804c1f4569fd3775e21418665b859a0ca6
Jonathan Stroud <stroud@umich.edu>
2019-04-05 23:24:00 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Drug review dataset: https://archive.ics.uci.edu/ml/datasets/Drug+Review+Dataset+%28Drugs.com%29#',
+    author='jonathancstroud',
+    license='MIT',
+)
